f385d2a6d1c97ec7896576b8d15aa015b0f76003
aasimd <dongarkar21@gmail.com>
2022-11-18 01:55:39 +0530

    error handler added

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ function constructURL(text){
     var url2 = url + "?text=" + text;
     return url2;
 }
-
+function errorHandler(error){
+    console.log("error occured ", error);
+    alert('there was an error, try again after sometime.')
+}
 function buttonclicked(){
     var text = inputText.value;
     fetch(constructURL(text))
     .then(response=>response.json())
     .then(json=> {
         var translatedText = json.contents.translated;
-        outputBox.innerText = translatedText
+        outputBox.innerText = translatedText;
     })
+    .catch(errorHandler);
 }
 
 
