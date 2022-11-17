623d725f81c8226ecf26f05178734777e02ea802
aasimd <dongarkar21@gmail.com>
2022-11-17 20:37:57 +0530

    done with js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var buttonClicked = document.querySelector(".btn")
+var translateButton = document.querySelector(".translate-button");
+var inputText = document.querySelector("#txt-area")
+var outputBox = document.querySelector(".outputbox")
+var text = inputText.value;
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text){
+    var url2 = url + "?text=" + text;
+    return url2;
+}
+
+function buttonclicked(){
+    var text = inputText.value;
+    fetch(constructURL(text))
+    .then(response=>response.json())
+    .then(json=> {
+        var translatedText = json.contents.translated;
+        outputBox.innerText = translatedText
+    })
+}
 
 
-buttonClicked.addEventListener("click", function button() {
-    console.log(a)
-})
+translateButton.addEventListener("click", buttonclicked );
+
